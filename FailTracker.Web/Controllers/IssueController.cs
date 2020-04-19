9203bf47e2168117a5bc3bad9cd06515f2222296
Marco Carcini <marco.carcini@hotmail.com>
2020-04-19 12:40:03 -0500

    Added a nested container, Container-Per-Request

---
+++ FailTracker.Web/Controllers/IssueController.cs
@@ -0,0 +1,21 @@
+﻿using FailTracker.Web.Data;
+using System.Web.Mvc;
+
+namespace FailTracker.Web.Controllers
+{
+    public class IssueController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IssueController(ApplicationDbContext context)
+        {
+            _context = context;            
+        }
+
+        public ActionResult Index()
+        {
+            return Content("Here is where issues would go!");
+        }
+
+    }
+}
