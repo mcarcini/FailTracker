2d7c4b65df17d3c843cf1c15b67a7b859e86cffc
Marco Carcini <marco.carcini@hotmail.com>
2020-06-13 13:50:18 -0500

    Implementing the interface IRunAtStartup

---
+++ FailTracker.Web/App_Start/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using FailTracker.Web.Data;
+using FailTracker.Web.Domain;
+using FailTracker.Web.Infrastructure.Tasks;
+using System.Linq;
+
+namespace FailTracker.Web.App_Start
+{
+    public class SeedData : IRunAtStartup
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SeedData(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public void Execute()
+        {
+            if (!_context.Users.Any()) {
+                _context.Users.Add(new ApplicationUser
+                {
+                    UserName = "TestUser"
+                });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
